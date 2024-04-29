96db413b7587d9c7cf1934526d41a206c41cfc46
Anil Chapagain <anilchapagain68@gmail.com>
2024-04-30 01:08:03 +0530

    cors new

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,7 @@
 const corsOption = {
   origin: [
     "*",
+    "https://chat-client-alpha-bice.vercel.app",
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
