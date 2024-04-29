7f4ed604ad117607251f9ef5244458ce98debc1c
Anil Chapagain <anilchapagain68@gmail.com>
2024-04-29 23:33:06 +0530

    cors added

--- constants/config.js
+++ constants/config.js
@@ -1,10 +1,10 @@
 const corsOption = {
   origin: [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:4173",
+    "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
